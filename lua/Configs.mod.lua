ff9ad4e67effbfc70ccf20f7629f00fe6162356e
Jeffrayeee <60801205+Jeffrayeee@users.noreply.github.com>
2022-07-01 17:02:33 -0400

    Update Configs.mod.lua

--- lua/Configs.mod.lua
+++ lua/Configs.mod.lua
@@ -3,5 +3,5 @@
 return {
 	-- NOTE: The final '/' in the URL is very important!
 	["BaseUrl"] = "http://test-app.glitch.me/"; -- The base URL of your deployed server. Example: http://test-app.glitch.me/ 
-	["AUTH_KEY"] = "SECRET_REQUEST_AUTH_KEY"; -- Secret Key defined as 'auth_key' in the config.json file of your server
+	["AUTH_KEY"] = "sadfklsadljkfhafiuhwe"; -- Secret Key defined as 'auth_key' in the config.json file of your server
 }
